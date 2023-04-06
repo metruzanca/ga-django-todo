ceab57d733fb6e67f66be2a8db7a17617d4fe7bf
Sam Zanca <sam@zanca.dev>
2023-04-06 10:17:53 -0400

    jwt again

--- project/settings.py
+++ project/settings.py
@@ -45,15 +45,15 @@ INSTALLED_APPS = [
     'todo',
 ]
 
-# REST_FRAMEWORK = {
-#     # 'DEFAULT_FILTER_BACKENDS': [
-#     #     'django_filters.rest_framework.DjangoFilterBackend',
-#     # ],
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         # 'django.contrib.auth.backends.ModelBackend',
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
