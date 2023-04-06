7bf02335a450417f4ecbb1cd8821f8e7fc94652a
Sam Zanca <sam@zanca.dev>
2023-04-06 11:17:14 -0400

    Kill jwt + model improvements

--- project/settings.py
+++ project/settings.py
@@ -46,13 +46,13 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.SessionAuthentication',
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
 }
 
 MIDDLEWARE = [
--- todo/models.py
+++ todo/models.py
@@ -1,19 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.conf import settings
-
-class Item(models.Model):
-    name = models.CharField(max_length=50)
-    done = models.BooleanField(default=False)
-    list = models.ForeignKey('List', on_delete=models.CASCADE, related_name='items')
-
-    def __str__(self):
-        return self.name
+from django.contrib.auth.models import User # This is re-exported
 
 class List(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='lists')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lists')
     # is_template = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    done = models.BooleanField(default=False)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name='items')
 
     def __str__(self):
         return self.name
